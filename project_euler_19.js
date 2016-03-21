91fc12e47bc49dd7cfa474165ff4927aef3b34d7
fabaldoni <fabaldoni@hotmail.com>
2016-03-21 10:49:01 -0400

    removed console.logs and reformatted

--- project_euler_19.js
+++ project_euler_19.js
@@ -12,30 +12,23 @@
  How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
  */
 
-function isLeapYear(year){
-
-    if(year%100===0){
+function isLeapYear(year) {
+    if (year % 100 === 0) {
         return year % 400 == 0;
     }
-    else{
+    else {
         return year % 4 === 0;
     }
-
 }
 
-//console.log(isLeapYear(1900));
-//console.log(isLeapYear(1901));
-//console.log(isLeapYear(-3.5));
 
-
-
-function daysInMonth(month,year){
-    switch(month){
+function daysInMonth(month, year) {
+    switch (month) {
         case 1: //january
             return 31;
             break;
         case 2: //february
-            return  isLeapYear(year)?29:28;
+            return isLeapYear(year) ? 29 : 28;
             break;
         case 3: //march
             return 31;
@@ -69,37 +62,27 @@ function daysInMonth(month,year){
             break;
     }
 }
-//console.log(daysInMonth(2,1900));
-//console.log(daysInMonth(2,1932));
 
-var results =[];
+var results = [];
 var day_of_week = 0;
 
+for (var y = 1900; y <= 2000; y++) {
 
-for(var y=1900;y<=2000;y++){
-    //console.log(y);
+    for (var m = 1; m <= 12; m++) {
 
-    for(var m=1; m<=12;m++){
-        //console.log(m);
-
-        //console.log("m=" + m + " days in month=" + daysInMonth(m,y))
-        for(var d=1; d<=daysInMonth(m,y); d++){
+        for (var d = 1; d <= daysInMonth(m, y); d++) {
             day_of_week++;
-            //console.log(day_of_week);
-            //console.log(d);
-            //console.log(" y=" + y + " m=" + m + " d=" + d + " weekday=" + day_of_week);
-            if(day_of_week === 7 && d===1){
-                if(y > 1900){
-                    results.push({"year":y,"month":m,"day":d,"weekday":day_of_week});
+            if (day_of_week === 7 && d === 1) {
+                if (y > 1900) {
+                    results.push({"year": y, "month": m, "day": d, "weekday": day_of_week});
                 }
             }
 
-            if(day_of_week===7){
+            if (day_of_week === 7) {
                 day_of_week = 0;
             }
         }
     }
 }
 
-//console.log(results);
 console.log(results.length);
